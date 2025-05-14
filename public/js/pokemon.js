18507039eba8b2933fa3097b33e208bc7bcb59de
raphaelmakaryan <contact@raphaelmakaryan.fr>
2025-05-14 13:46:31 +0200

    feat: Dark/Light Mode

--- public/js/pokemon.js
+++ public/js/pokemon.js
@@ -15,6 +15,29 @@ function searchPokemonArray(tab, search) {
 }
 //#endregion SEARCHPOKEMONARRAY
 
+//#region DARK/LIGHT THEME
+document.addEventListener('DOMContentLoaded', function () {
+
+    const themeToggle = document.getElementById('theme-toggle');
+    const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)');
+
+    const currentTheme = localStorage.getItem('theme');
+    if (currentTheme) {
+        document.documentElement.setAttribute('data-theme', currentTheme);
+    } else if (prefersDarkScheme.matches) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+    }
+
+    themeToggle.addEventListener('click', function () {
+        const currentTheme = document.documentElement.getAttribute('data-theme');
+        const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+        document.documentElement.setAttribute('data-theme', newTheme);
+        localStorage.setItem('theme', newTheme);
+    });
+
+});
+//#endregion DARK/LIGHT THEME
+
 //#region STOCK ID
 function stockId(id) {
     const getLastID = localStorage.getItem("lastIdPokemon");
